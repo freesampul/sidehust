991d483b1c0112db17c16519212dd000fd780e4f
sqeejee <98850954+sqeejee@users.noreply.github.com>
2024-03-07 21:13:32 -0800

    who wouldn't want to buy this?

--- src/components/payment/payment.component.jsx
+++ src/components/payment/payment.component.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
 import { getCheckoutUrl } from "../../utils/firebase/firebase.utils";
-import { firebaseApp, validateUserSubscription } from "../../utils/firebase/firebase.utils"; // Ensure you export `firebaseApp`
+import { firebaseApp } from "../../utils/firebase/firebase.utils"; // Ensure you export `firebaseApp`
 
 const Payment = () => {
     const [selectedTier, setSelectedTier] = useState("Tier 1");
 
-    const handleTierChange = (event) => {
-        setSelectedTier(event.target.value);
-    };
 
     const handlePayment = async () => {
         try {
@@ -27,31 +24,56 @@ const Payment = () => {
         }
     };
 
-    // const printStatus = async () => {
-    //     try {
-    //         const status = await getPremiumStatusAndUpdatePoints(firebaseApp);
-    //         console.log("Premium status", status);
-    //     } catch (error) {
-    //         console.log("Error getting premium status", error);
-    //     }
-    // }
-
-    const validateUser = async () => {
-        validateUserSubscription(firebaseApp, "userId");
-    }
-
     return (
-        <div>
-            <h1>Payment</h1>
-            <select value={selectedTier} onChange={handleTierChange}>
-                <option value="Tier 1">Tier 1</option>
-                <option value="Tier 2">Tier 2</option>
-                <option value="Tier 3">Tier 3</option>
-            </select>
-            <button onClick={handlePayment}>Pay</button>
-            <button onClick={validateUser}>Status</button>
-        </div>
-        
+        <>
+            <div className="h-screen relative">
+                <div className="h-screen bg-gradient-to-b from-red-50 to-white-100 absolute top-0 left-0 right-0"></div>
+                <div className="flex flex-col items-center justify-center h-screen relative">
+                    <div className="max-w-lg px-4">
+                        <h1 className="text-4xl md:text-6xl font-bold text-center mb-8">Plans for everyone.</h1>
+                        <p className="text-lg text-center">Start learning today for completely free</p>
+                    </div>
+                    <div className="grid grid-cols-3 gap-8 mt-8 mx-auto w-4/5 outline-gray-100 p-8 rounded-lg">
+                        <div className="bg-gray-200 rounded-xl shadow-lg p-8">
+                            <h2 className="text-xl font-bold mb-4">Basic</h2>
+                            <h2 className="text-4xl font-bold mb-4">Free</h2>
+                            <h3 className="text-sm font-bold mb-4 text-gray-600">0 tokens</h3>
+                            <br></br>
+                            <br></br>
+                            <p className="text-md mb-4">Access to two courses</p>
+                            <p className="text-md mb-4">Earn tokens by helping other users</p>
+                            <p className="text-md mb-4">Limited access to ai tools & money makers</p>
+                            <br></br>
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Free</button>
+                        </div>
+                        <div className="bg-red-500 text-white rounded-xl shadow-lg p-8">
+                            <h2 className="text-xl font-bold mb-4">Idk</h2>
+                            <h2 className="text-4xl font-bold mb-4">$2/month</h2>
+                            <h3 className="text-sm font-bold mb-4 text-black">100 tokens</h3>
+                            <br></br>
+                            <br></br>
+                            <p className="text-md mb-4">Access to all courses</p>
+                            <p className="text-md mb-4">Full access to ai tools</p>
+                            <p className="text-md mb-4">Explore money making guides and start earning</p>
+                            <br></br>
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handlePayment}>Upgrade</button>
+                        </div>
+                        <div className="bg-gray-200 rounded-xl shadow-lg p-8">
+                            <h2 className="text-xl font-bold mb-4">Pro</h2>
+                            <h2 className="text-4xl font-bold mb-4">$5/month</h2>
+                            <h3 className="text-sm font-bold mb-4 text-gray-600">250 tokens</h3>
+                            <br></br>
+                            <br></br>
+                            <p className="text-md mb-4">Access to all courses</p>
+                            <p className="text-md mb-4">Full access to ai tools</p>
+                            <p className="text-md mb-4">Explore money making guides and start earning</p>
+                            <br></br>
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handlePayment}>Upgrade</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
